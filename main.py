eb8e7cd51ce61b0284972c81c17b8137f98e9d5b
demo <abhishekshinde5805@gmail.com>
2026-06-18 21:32:29 +0530

    adding all files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# AI Real-Time Gym Coach this is the main file for the streamlit app. It will import the necessary libraries and set up the page configuration. It will also display the title and a brief description of the app.
 import streamlit as st
 
 st.set_page_config(page_title="AI Real-Time Gym Coach")
