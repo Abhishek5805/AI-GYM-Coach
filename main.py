84a675159dd9230ab30c6178c71a2181ff1477e6
demo <abhishekshinde5805@gmail.com>
2026-06-20 21:19:08 +0530

    add new file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # AI Real-Time Gym Coach this is the main file for the streamlit app. It will import the necessary libraries and set up the page configuration. It will also display the title and a brief description of the app.
 import streamlit as st
+from services.auth.login_wall import render_login_wall
 
-st.set_page_config(page_title="AI Real-Time Gym Coach")
+def main():
+    st.set_page_config(
+        page_icon="🏋️‍♂️",
+        page_title="⛹️AI Real-Time Gym Coach",
+        initial_sidebar_state="expanded",
+        layout="centered",
+    )
 
-st.title("AI Real-Time Gym Coach")
-st.write("setup done")
+    if not render_login_wall():
+        return  # Stop further execution if the user is not logged in
+    st.write("hello")
+if __name__ == "__main__":
+    main()
